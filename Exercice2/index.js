72b2be116ace2882fdaab761b3f59c1c51913244
LeSheitanWilo <sinkamwilfried@gmail.com>
2021-05-30 20:18:09 +0200

    Test-- Evok
    Test-- Evok

---
+++ Exercice2/index.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+   let bg = document.getElementsByClassName('bg1')[0];
+    let sta = document.getElementsByClassName('static')[0];
+    let text = document.getElementsByClassName('texte')[0];
+   bg.classList.toggle('para');
+   sta.classList.toggle('stat');
+   text.classList.toggle('txt');
+}
